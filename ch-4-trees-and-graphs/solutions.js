e430089829ed886e79ec4e3022b92d3268359b8c
Mike Luz <onlymikeluz@gmail.com>
2017-06-26 21:04:27 -0400

    fixed 4.2 and 4.3

--- ch-4-trees-and-graphs/solutions.js
+++ ch-4-trees-and-graphs/solutions.js
@@ -55,34 +55,24 @@ function createBST(array) {
 	var arrCopy = [].slice.call(array);
 	var tree;
 	var rootIndex = Math.floor(arrCopy.length / 2);
-
+	
+	var rootEl = arrCopy[rootIndex];
+	tree = new BST(rootEl);
+	
 	// add left
-	for (var i = rootIndex; i >= 0; i -= 2) {
-		if (i === rootIndex) {
-			var rootEl = arrCopy[i];
-			tree = new BST(rootEl);
-			// check in case array has only one element, do not add i-1
-			if (arrCopy[i-1]) {
-				tree.push(arrCopy[i-1]);
-			}
-		} else {
-			if (arrCopy[i-1]) {
-				tree.push(arrCopy[i+1]);
-			} else {
-				tree.push(arrCopy[i+1]);
-				tree.push(arrCopy[i]);
-			}
+	for (var i = rootIndex - 1; i >= 0; i -= 2) {
+		if (arrCopy[i-1]) {
+			tree.push(arrCopy[i-1]);
 		}
+		tree.push(arrCopy[i]);
 	}
 
 	// add right
 	for (var i = rootIndex + 1; i < arrCopy.length; i += 2) {
 		if (arrCopy[i+1]) {
 			tree.push(arrCopy[i+1]);
-			tree.push(arrCopy[i])
-		} else {
-			tree.push(arrCopy[i]);
 		}
+		tree.push(arrCopy[i]);
 	}
 
 	return tree;
@@ -132,19 +122,14 @@ function createLinkedLists(rootNode, arrayOfLists, level) {
 		return;
 	}
 	var list = null;
-	if (level === arrayOfLists.length) {
+	if (!arrayOfLists[level]) {
 		arrayOfLists.push(new LinkedList());
-		list = arrayOfLists[level];
-	} else {
-		list = arrayOfLists[level];
 	}
-	list.addToHead(rootNode);
+	list = arrayOfLists[level];
+	list.addToHead(rootNode.value);
 	createLinkedLists(rootNode.left, arrayOfLists, level + 1);
 	createLinkedLists(rootNode.right, arrayOfLists, level + 1);
 }
 var array = [];
 createLinkedLists(newBSTOdd, array, 0);
 console.log(array);
-
-
-
